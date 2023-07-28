cfaff491474fb9d7a437a97d9d9d1f9a4d87bd78
dylan farrell <Dylancf01@gmail.com>
2023-07-27 23:00:37 -0400

    Fixed Bug

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './nav.css'
 import {AiOutlineHome} from 'react-icons/ai'
-import {GrProjects} from 'react-icons/gr'
+
 import {BiBook} from 'react-icons/bi'
 import {useState} from 'react'
 
@@ -9,7 +9,7 @@ const Nav = () => {
   const[activeNav, setActiveNav] = useState('#')
   return (
     <nav>
-      <a href="#" onClick={() => setActiveNav('#')} className={activeNav === '#' ? 'active': ''}><AiOutlineHome/></a>
+      <a href="#header" onClick={() => setActiveNav('#header')} className={activeNav === '#header' ? 'active': ''}><AiOutlineHome/></a>
       <a href="#experience" onClick={() => setActiveNav('#experience')} className={activeNav === '#experience' ? 'active' : ''}><BiBook/></a>
 
    
